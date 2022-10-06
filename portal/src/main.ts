f8c2ab1c91af4fc6d82fed383875d4228b096388
Fernando Halmenschlager Filter <fernandohfilter10@gmail.com>
2022-10-05 21:28:15 -0300

    Project Configuration and Search Bar

--- portal/src/main.ts
+++ portal/src/main.ts
@@ -1,10 +1,28 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import App from './App.vue'
+import router from './router'
 
-Vue.config.productionTip = false;
+Vue.use(Vuetify)
+
+const opts = {
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: '#1c1c1c'
+      }
+    }
+  }
+}
+
+const vuetify: Vuetify = new Vuetify(opts)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: (h) => h(App),
-}).$mount("#app");
+  vuetify,
+  render: (h) => h(App)
+}).$mount('#app')
---
+++ portal/src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="main">
+    <div class="search">
+      <v-text-field
+        v-model="text"
+        class="search_field"
+        dark
+        label="Digite sua busca"
+      />
+      <v-btn class="search_btn" color="#005ba5"> Pesquisar </v-btn>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      text: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.main {
+  width: 80%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: #474747;
+}
+
+.search {
+  display: flex;
+  align-items: center;
+
+  &_field {
+    margin-right: 15px;
+  }
+
+  &_btn {
+    color: #fff !important;
+  }
+}
+</style>
